f3471516a57dadacf29b536ace200f51734e744c
fkwilken <fkwilken@gmail.com>
2025-01-08 09:59:51 -0800

    Remove Wayland
    Signed-off-by: fkwilken <fkwilken@gmail.com>

--- run.sh
+++ run.sh
@@ -41,9 +41,6 @@ else
                 -v /tmp/.X11-unix:/tmp/.X11-unix \
                 -v /mnt/wslg:/mnt/wslg \
                 -e DISPLAY \
-                -e WAYLAND_DISPLAY \
-                -e XDG_RUNTIME_DIR \
-                -e PULSE_SERVER \
                 -e "TERM=$TERM"\
                 --hostname $HOST \
                 --net=host \
