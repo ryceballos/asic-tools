4221bd14947aef9775bb73683e28849e81c70422
fkwilken <fkwilken@gmail.com>
2025-01-27 10:12:39 -0800

    Make Workspace Absolute Dirs
    Signed-off-by: fkwilken <fkwilken@gmail.com>

--- run.sh
+++ run.sh
@@ -1,3 +1,8 @@
+#!/usr/bin/env bash
+
+# Get Current Directory
+ABS_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" && pwd )
+
 # Select Local or Remote Container
 if [ "$1" = "-l" ] || [ "$1" = "--local" ]; then
     CONTAINER=asic
@@ -8,8 +13,8 @@ fi
 TERM=xterm-256color
 HOST=asic
 
-if [ ! -d workspace ]; then
-	mkdir -p workspace
+if [ ! -d $ABS_DIR/workspace ]; then
+	mkdir -p $ABS_DIR/workspace
 fi
 
 
@@ -22,7 +27,7 @@ if [[ "$OSTYPE" == "darwin"* ]]; then
 
     docker run -it --rm \
 	--platform linux/amd64 \
-        -v $(pwd)/workspace:/home/$HOST/workspace:rw \
+        -v $ABS_DIR/workspace:/home/$HOST/workspace:rw \
         -v ~/.ssh:/home/$HOST/.ssh\
         -v ~/.gitconfig:/home/$HOST/.gitconfig\
         -e DISPLAY=host.docker.internal:0 \
@@ -35,7 +40,7 @@ else
     set -x
     xhost local:root
     docker run -it --rm \
-                -v $(pwd)/workspace:/home/$HOST/workspace:rw\
+                -v $ABS_DIR/workspace:/home/$HOST/workspace:rw\
                 -v ~/.ssh:/home/$HOST/.ssh\
                 -v ~/.gitconfig:/home/$HOST/.gitconfig\
                 -v /tmp/.X11-unix:/tmp/.X11-unix \
